e91d68ad1f507796b9c3f0058dd2e095de30dbbc
Elizabeth Morales <elizabeth.morales@ekupd.com>
2021-04-27 18:10:00 +0200

    make unit test to directive using the linechart component

--- src/app/common/directives/responsive-width/responsive-width.directive.spec.ts
+++ src/app/common/directives/responsive-width/responsive-width.directive.spec.ts
@@ -1,8 +1,43 @@
+import { DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { LineChartComponent } from 'src/app/shared/line-chart/line-chart.component';
 import { ResponsiveWidthDirective } from './responsive-width.directive';
+import { By } from '@angular/platform-browser';
 
 describe('ResponsiveWidthDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ResponsiveWidthDirective();
-    expect(directive).toBeTruthy();
+  let component: LineChartComponent;
+  let fixture: ComponentFixture<LineChartComponent>;
+  let debugElement: DebugElement;
+
+  beforeEach(() => {
+    fixture = TestBed.configureTestingModule({
+      declarations: [LineChartComponent, ResponsiveWidthDirective]
+    }).createComponent(LineChartComponent);
+    debugElement = fixture.debugElement;
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should have one div with the directive', () => {
+    const div = fixture.debugElement.queryAll(By.directive(ResponsiveWidthDirective));
+    expect(div.length).toBe(1);
+  });
+
+  it('should change div width to 500px', () => {
+    component.viewWidth = 500;
+    fixture.detectChanges();
+
+    const div = fixture.debugElement.query(By.directive(ResponsiveWidthDirective));
+
+    expect(div.nativeElement.style.width).toBe('500px');
+  });
+
+  it('should set unset value to width when 0', () => {
+    component.viewWidth = 0;
+    fixture.detectChanges();
+
+    const div = fixture.debugElement.query(By.directive(ResponsiveWidthDirective));
+
+    expect(div.nativeElement.style.width).toBe('unset');
   });
 });
