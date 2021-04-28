1109f459f6a17278073920a0f68c6e0bd6d87e73
Elizabeth Morales <eliza3291@gmail.com>
2021-04-28 13:51:04 +0200

    add comment

--- src/app/common/data/index.ts
+++ src/app/common/data/index.ts
@@ -1,3 +1,7 @@
+/**
+ * All data needed for the components to work.
+ * Values can be changed as needed.
+ */
 export * from './map-paths.data';
 export * from './timeframe.data';
 export * from './line-chart.data';
