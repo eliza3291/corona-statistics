5d1c2bce7436dc8a3ab7e8b94d41f5092d990118
Elizabeth Morales <eliza3291@gmail.com>
2021-05-01 20:34:13 +0200

    update build app and reorganize mock data for tests

--- src/app/common/mocks/germany.mock.ts
+++ src/app/common/mocks/germany.mock.ts
@@ -3,40 +3,10 @@ import { Timeseries, TimeseriesState } from '../models';
 /** Example of a mapped response from the endpoint with the data of Germany */
 export const MAPPED_GERMANY_RESPONSE: Timeseries = Timeseries.initializeClass([
 	{
-		cases: 7638,
-		deaths: 8,
-		recovered: 51,
-		date: 'April 26'
-	},
-	{
-		cases: 8388,
-		deaths: 5,
-		recovered: 56,
-		date: 'April 25'
-	},
-	{
-		cases: 17562,
-		deaths: 8,
-		recovered: 156,
-		date: 'April 24'
-	},
-	{
-		cases: 22651,
-		deaths: 17,
-		recovered: 286,
-		date: 'April 23'
-	},
-	{
-		cases: 25287,
-		deaths: 21,
-		recovered: 396,
-		date: 'April 22'
-	},
-	{
-		cases: 29453,
-		deaths: 29,
-		recovered: 820,
-		date: 'April 21'
+		cases: 12343,
+		deaths: 35,
+		recovered: 977,
+		date: 'April 19'
 	},
 	{
 		cases: 27983,
@@ -45,49 +15,50 @@ export const MAPPED_GERMANY_RESPONSE: Timeseries = Timeseries.initializeClass([
 		date: 'April 20'
 	},
 	{
-		cases: 12343,
-		deaths: 35,
-		recovered: 977,
-		date: 'April 19'
-	}
-]);
-/** Example of a mapped response from the endpoint with the data of the state with id 1*/
-export const MAPPED_STATE_RESPONSE: TimeseriesState = TimeseriesState.initializeClass(1, [
-	{
-		cases: 104,
-		deaths: 0,
-		recovered: 1,
-		date: 'April 26'
+		cases: 29453,
+		deaths: 29,
+		recovered: 820,
+		date: 'April 21'
 	},
 	{
-		cases: 207,
-		deaths: 0,
-		recovered: 2,
-		date: 'April 25'
-	},
-	{
-		cases: 381,
-		deaths: 1,
-		recovered: 4,
-		date: 'April 24'
-	},
-	{
-		cases: 491,
-		deaths: 0,
-		recovered: 5,
-		date: 'April 23'
-	},
-	{
-		cases: 519,
-		deaths: 0,
-		recovered: 13,
+		cases: 25287,
+		deaths: 21,
+		recovered: 396,
 		date: 'April 22'
 	},
 	{
-		cases: 817,
-		deaths: 1,
-		recovered: 39,
-		date: 'April 21'
+		cases: 22651,
+		deaths: 17,
+		recovered: 286,
+		date: 'April 23'
+	},
+	{
+		cases: 17562,
+		deaths: 8,
+		recovered: 156,
+		date: 'April 24'
+	},
+	{
+		cases: 8388,
+		deaths: 5,
+		recovered: 56,
+		date: 'April 25'
+	},
+	{
+		cases: 7638,
+		deaths: 8,
+		recovered: 51,
+		date: 'April 26'
+	}
+]);
+
+/** Example of a mapped response from the endpoint with the data of the state with id 1*/
+export const MAPPED_STATE_RESPONSE: TimeseriesState = TimeseriesState.initializeClass(1, [
+	{
+		cases: 260,
+		deaths: 3,
+		recovered: 24,
+		date: 'April 19'
 	},
 	{
 		cases: 536,
@@ -96,9 +67,39 @@ export const MAPPED_STATE_RESPONSE: TimeseriesState = TimeseriesState.initialize
 		date: 'April 20'
 	},
 	{
-		cases: 260,
-		deaths: 3,
-		recovered: 24,
-		date: 'April 19'
+		cases: 817,
+		deaths: 1,
+		recovered: 39,
+		date: 'April 21'
+	},
+	{
+		cases: 519,
+		deaths: 0,
+		recovered: 13,
+		date: 'April 22'
+	},
+	{
+		cases: 491,
+		deaths: 0,
+		recovered: 5,
+		date: 'April 23'
+	},
+	{
+		cases: 381,
+		deaths: 1,
+		recovered: 4,
+		date: 'April 24'
+	},
+	{
+		cases: 207,
+		deaths: 0,
+		recovered: 2,
+		date: 'April 25'
+	},
+	{
+		cases: 104,
+		deaths: 0,
+		recovered: 1,
+		date: 'April 26'
 	}
 ]);
