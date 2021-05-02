ead24f71796238ccecb7da293b3c241e2607a5e4
Elizabeth Morales <eliza3291@gmail.com>
2021-05-02 13:59:19 +0200

    add e2e tests

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,35 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, ElementFinder, WebElement } from 'protractor';
 
 export class AppPage {
   async navigateTo(): Promise<unknown> {
     return browser.get(browser.baseUrl);
   }
 
-  async getTitleText(): Promise<string> {
-    return element(by.css('app-root .content span')).getText();
+  async getTitleOverview(): Promise<string> {
+    return element(by.css('app-root .container-fluid .card h1.card-title')).getText();
+  }
+
+  async getCasesText(): Promise<string> {
+    return element(by.css('.fas.fa-viruses')).element(by.xpath('..')).element(by.css('span.display-4')).getText();
+  }
+
+  async getDeathsText(): Promise<string> {
+    return element(by.css('.fas.fa-cross')).element(by.xpath('..')).element(by.css('span.display-4')).getText();
+  }
+
+  async getRecoveredText(): Promise<string> {
+    return element(by.css('.fas.fa-hand-holding-heart')).element(by.xpath('..')).element(by.css('span.display-4')).getText();
+  }
+
+  async getNextButton(): Promise<WebElement> {
+    return element(by.css('app-timeframe-selector #btnNext')).getWebElement();
+  }
+
+  async getBackButton(): Promise<WebElement> {
+    return element(by.css('app-timeframe-selector #btnBack')).getWebElement();
+  }
+
+  async getGermanyMapState1(): Promise<WebElement> {
+    return element(by.css('app-germany-map .land[id="1"]')).getWebElement();
   }
 }
